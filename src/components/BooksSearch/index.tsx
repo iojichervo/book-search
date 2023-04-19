097c2079b49ea5a344b31df731cee9bf859c66f0
Ioji Chervonagura <ioji@chervonagura.com.ar>
2023-04-19 17:15:38 -0300

    Minor UI improvements

--- src/components/BooksSearch/index.tsx
+++ src/components/BooksSearch/index.tsx
@@ -4,12 +4,14 @@ import { useState } from 'react'
 
 const { Search } = Input
 
+const MIN_LENGTH_SEARCH = 3
+
 export default function BooksSearch() {
   const [isInvalid, setIsInvalid] = useState<boolean>(false)
   const [search, setSearch] = useState<string>()
 
   const onSearch = (value: string) => {
-    if (value.length > 3) {
+    if (value.length > MIN_LENGTH_SEARCH) {
       setSearch(value)
       setIsInvalid(false)
     } else {
@@ -26,10 +28,15 @@ export default function BooksSearch() {
           onSearch={onSearch}
           enterButton
         />
-        {isInvalid && <small>The searched value needs to be longer</small>}
+        {isInvalid && (
+          <small>
+            The searched value needs have at least {MIN_LENGTH_SEARCH}{' '}
+            characters
+          </small>
+        )}
       </div>
 
-      {!search && <Empty />}
+      {!search && <Empty description={'No books have been searched'} />}
       {search && <BooksTable search={search} />}
     </>
   )
